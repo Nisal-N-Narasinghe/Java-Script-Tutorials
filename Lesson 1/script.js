b6d236705dc6c0c596754ac4d3bbbff5869cddee
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-04 23:26:41 +0530

    Lesson 1

---
+++ Lesson 1/script.js
@@ -0,0 +1,17 @@
+//Display alert window
+alert("Hello Nisal");
+
+//Declaring variable and assign value for it
+var fname = "Nisal";
+
+//Display varible value in console
+console.log(fname);
+//Display varible value in webpage
+document.write(fname);
+//Use line break 
+document.write("<br>");
+
+//calculating
+var Sum;
+Sum = 5 + 5;
+document.write(Sum);
--- my-first-java-script.js
+++
@@ -1,8 +0,0 @@
-//Display alert window
-alert("Hello world!");
-
-//Declaring variable and assign value for it
-var fname = "Nisal"
-
-//Display varible value
-console.log(fname);
