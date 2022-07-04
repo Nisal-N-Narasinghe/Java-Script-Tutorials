879cea6667932aa90c6da78cb4fbd115301587e3
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-04 20:53:24 +0530

    Lesson 01

--- my-first-java-script.js
+++ my-first-java-script.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+//Display alert window
+alert("Hello world!");
+
+//Declaring variable and assign value for it
+var fname = "Nisal"
+
+//Display varible value
+console.log(fname);
