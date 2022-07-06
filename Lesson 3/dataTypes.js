58521cc89cd792d3c18b1784b71b336ba8bf82ed
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-06 11:40:48 +0530

    Lesson 3

---
+++ Lesson 3/dataTypes.js
@@ -0,0 +1,35 @@
+/*
+Data Types in Js
+
+Primitive          Non-Primitive
+ *String            *Array
+ *Number            *Object
+ *Boolean
+ *Null
+ *Undefined
+
+*/
+
+//String Data Type
+var firstName = "Nisal";
+
+//Number Data Type
+var age = 25;
+
+//Boolean Data Type
+var married = false;
+
+//Null Data Type
+var middleName = null;
+
+//Undefined Data Type
+var lastName;
+
+//We also can declaring and assign variables by using the below method
+var firstName = "Nisal",
+    age = 25,
+    married = false,
+    middleName = null,
+    lastName;
+
+document.write(firstName + "<br> " + age + "<br>" + middleName + "<br>" + lastName);
