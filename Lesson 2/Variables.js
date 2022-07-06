04ede816b40eec9921ca1d7ea45e9ed10783a475
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-06 11:14:22 +0530

    Lesson 2

---
+++ Lesson 2/Variables.js
@@ -0,0 +1,15 @@
+//declaring a variable
+var myScore;
+
+//assign value to variable
+myScore = 150;
+
+//creating a new variable starting with underscore
+var _Subject;
+
+//display variable value
+document.write(myScore);
+
+//Java script variables are case sensitive
+//A variable can not stat with a number or a special characters
+//We cant use reserved words like function for variable names
