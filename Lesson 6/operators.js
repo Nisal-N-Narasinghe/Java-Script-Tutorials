2c1260f515ab521bc8b5a2719c7e5fb5d55f0e1c
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-06 13:38:19 +0530

    Lesson 6

---
+++ Lesson 6/operators.js
@@ -0,0 +1,14 @@
+//Creating a tables for showing types of operators
+document.write("<table border='2' cellspacing='0' cellpadding='6'>",
+    "<tr><th>Operator</th><th>Name</th><th>Example</th><th>Result</th></tr>",
+    "<tr><td>+</td><td>Addition</td><td>5+4</td><td>9</td></tr>",
+    "<tr><td>-</td><td>Substraction</td><td>6-2</td><td>2</td></tr>",
+    "<tr><td>*</td><td>Multiplication</td><td>3*4</td><td>12</td></tr>",
+    "<tr><td>/</td><td>Division</td><td>10/5</td><td>2</td></tr>",
+    "<tr><td>%</td><td>Modulus</td><td>6%4</td><td>2</td></tr>",
+    "<tr><td>++</td><td>Return value and increment</td><td>x = 1; x++</td><td>2</td></tr>",
+    "<tr><td>++</td><td>Increment and return value</td><td>x = 1; ++x</td><td>3</td></tr>",
+    "<tr><td>--</td><td>Return value and decrement</td><td>x = 3; x--</td><td>3</td></tr>",
+    "<tr><td>--</td><td>Decrement and return value</td><td>x = 3; --x</td><td>2</td></tr>",
+    "</table>"
+);
