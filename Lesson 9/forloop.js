12696f825d0f925e4db6f76cfd93594f818d8aff
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-07 23:18:23 +0530

    Lesson 9

--- Lesson 9/forloop.js
+++ Lesson 9/forloop.js
@@ -1,4 +1,3 @@
-document.write = ("Hello");
 /*
 Syntax:
 
@@ -7,17 +6,25 @@ for (start; condition; step) {
 }
 */
 
+//Creat array called months for store months
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-document.write = (months[0] + "<br>");
-document.write = (months[1] + "<br>");
-document.write = (months[2] + "<br>");
-document.write = (months[3] + "<br>");
-document.write = (months[4] + "<br>");
-document.write = (months[5] + "<br>");
-document.write = (months[6] + "<br>");
-document.write = (months[7] + "<br>");
-document.write = (months[8] + "<br>");
-document.write = (months[9] + "<br>");
-document.write = (months[10] + "<br>");
-document.write = (months[11] + "<br>");
+document.write("<h2>Display months using normal method</h2>");
+document.write(months[0] + "<br>");
+document.write(months[1] + "<br>");
+document.write(months[2] + "<br>");
+document.write(months[3] + "<br>");
+document.write(months[4] + "<br>");
+document.write(months[5] + "<br>");
+document.write(months[6] + "<br>");
+document.write(months[7] + "<br>");
+document.write(months[8] + "<br>");
+document.write(months[9] + "<br>");
+document.write(months[10] + "<br>");
+document.write(months[11] + "<br>");
+
+
+document.write("<h2>Display months using foor loop</h2>");
+for (var i = 0; i < months.length; i++) {
+    document.write(months[i] + "<br>");
+}
