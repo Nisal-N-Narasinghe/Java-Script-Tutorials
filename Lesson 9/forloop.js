98a3aea5aa5fd7567806ea1a507f511ff69a21f5
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-07-07 23:07:31 +0530

    Lesson 9

---
+++ Lesson 9/forloop.js
@@ -0,0 +1,23 @@
+document.write = ("Hello");
+/*
+Syntax:
+
+for (start; condition; step) {
+    // code to be repeated
+}
+*/
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.write = (months[0] + "<br>");
+document.write = (months[1] + "<br>");
+document.write = (months[2] + "<br>");
+document.write = (months[3] + "<br>");
+document.write = (months[4] + "<br>");
+document.write = (months[5] + "<br>");
+document.write = (months[6] + "<br>");
+document.write = (months[7] + "<br>");
+document.write = (months[8] + "<br>");
+document.write = (months[9] + "<br>");
+document.write = (months[10] + "<br>");
+document.write = (months[11] + "<br>");
